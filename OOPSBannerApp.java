728a65f69660bc8c7e580edf95456f645a63bc05
Hemanth-20-2003 <hemux2000@gmail.com>
2026-02-18 23:06:25 +0530

    UC-6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,66 @@
 /**
- * OOPSBannerApp UC5 – OOPS Banner Display Application
+ * OOPSBannerApp UC6 – OOPS Banner Display Application
  * @author Developer
- * @version 5.0
- * This version directly initializes the banner lines inside a String array in single command using {}.
- * and then printing them row by row
+ * @version 6.0
+ * printing each letter from different methods inside a utility class.
  **/
 class OOPSBannerApp{
 	public static void main(String[] args){
 
-		// Storing all banner rows directly in an array
-		String lines[]={"     ***        ***     *******     ***** ",
-						"   **   **    **   **   **    *   **      ",
-						"  **     **  **     **  **    *  **       ",
-						"  **     **  **     **  **    *   **      ",
-						"  **     **  **     **  *******     ***   ",
-						"  **     **  **     **  **             ** ",
-						"  **     **  **     **  **              **",
-						"   **   **    **   **   **             ** ",
-						"     ***        ***     **        *****   "};
+		// Getting the pattern for each letter from the Util class
+		String letterO[]=Util.printOLetter();
+		String letterP[]=Util.printPLetter();
+		String letterS[]=Util.printSLetter();
 
-		// Looping through the array and printing each line
-		for(String line:lines){
-			System.out.println(line);
+		// Looping through each row and printing OOPS side by side
+		for(int i=0;i<9;i++){
+			System.out.println(letterO[i]+"  "+letterO[i]+"  "+letterP[i]+"  "+letterS[i]);
 		}
+	}
+}
+
+// Utility class that contains reusable methods(Letter patterns)
+class Util{
+
+	// Method to generate the letter 'O'
+	public static String[] printOLetter(){
+		String lines[]={"     ***   ",
+						"   **   ** ",
+						"  **     **",
+						"  **     **",
+						"  **     **",
+						"  **     **",
+						"  **     **",
+						"   **   ** ",
+						"     ***   "};
+		return lines;
+	}
+
+	// Method to generate the letter 'P'
+	public static String[] printPLetter(){
+		String lines[]={"*******",
+						"**    *",
+						"**    *",
+						"**    *",
+						"*******",
+						"**     ",
+						"**     ",
+						"**     ",
+						"**     "};
+		return lines;
+	}
+
+	// Method to generate the letter 'S'
+	public static String[] printSLetter(){
+		String lines[]={"   ***** ",
+						" **      ",
+						"**       ",
+						" **      ",
+						"   ***   ",
+						"      ** ",
+						"       **",
+						"      ** ",
+						" *****   "};
+		return lines;
 	}
 }
