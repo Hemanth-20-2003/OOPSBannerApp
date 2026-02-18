1974ed58b0a9c5871cac78755b356e8926967f06
Hemanth-20-2003 <hemux2000@gmail.com>
2026-02-18 22:15:22 +0530

    UC-1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ * @author Developer
+ * @version 1.0
+ * Purpose: Demonstrates a simple Java application that displays
+ *          the Object Oriented Programming System (OOPS) acronym
+ *          to the console.
+ **/
+class OOPSBannerApp{
+	public static void main(String[] args){
+		// Printing the word OOPS to the screen
+		System.out.println("OOPS");
+	}
+}
