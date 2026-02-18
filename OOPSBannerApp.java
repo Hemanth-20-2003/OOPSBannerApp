36182da9c7805e5842393125f47f5a8d3b82efa6
Hemanth-20-2003 <hemux2000@gmail.com>
2026-02-18 22:52:38 +0530

    UC-5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,25 @@
 /**
- * OOPSBannerApp UC4 – OOPS Banner Display Application
+ * OOPSBannerApp UC5 – OOPS Banner Display Application
  * @author Developer
- * @version 4.0
- * This version stores each row of the OOPS banner inside a String array and then prints it using a loop.
- * The banner rows are created using String.join()
+ * @version 5.0
+ * This version directly initializes the banner lines inside a String array in single command using {}.
+ * and then printing them row by row
  **/
 class OOPSBannerApp{
 	public static void main(String[] args){
 
-		//rows are stored in the String array
-		String lines[]=new String[9];
-		lines[0]=String.join("   ","   ***   ","   ***   ","*******","   ***** ");
-		lines[1]=String.join("   "," **   ** "," **   ** ","**    *"," **      ");
-		lines[2]=String.join("   ","**     **","**     **","**    *","**       ");
-		lines[3]=String.join("   ","**     **","**     **","**    *"," **      ");
-		lines[4]=String.join("   ","**     **","**     **","*******","   ***   ");
-		lines[5]=String.join("   ","**     **","**     **","**     ","      ** ");
-		lines[6]=String.join("   ","**     **","**     **","**     ","       **");
-		lines[7]=String.join("   "," **   ** "," **   ** ","**     ","      ** ");
-		lines[8]=String.join("   ","   ***   ","   ***   ","**     "," *****   ");
-		
-		//printing row by row
+		// Storing all banner rows directly in an array
+		String lines[]={"     ***        ***     *******     ***** ",
+						"   **   **    **   **   **    *   **      ",
+						"  **     **  **     **  **    *  **       ",
+						"  **     **  **     **  **    *   **      ",
+						"  **     **  **     **  *******     ***   ",
+						"  **     **  **     **  **             ** ",
+						"  **     **  **     **  **              **",
+						"   **   **    **   **   **             ** ",
+						"     ***        ***     **        *****   "};
+
+		// Looping through the array and printing each line
 		for(String line:lines){
 			System.out.println(line);
 		}
