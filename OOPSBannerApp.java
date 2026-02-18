e2780a226c2bbd4aee2574494a9e98e3d85845da
Hemanth-20-2003 <hemux2000@gmail.com>
2026-02-18 22:44:46 +0530

    UC-4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,28 @@
 /**
- * OOPSBannerApp UC3 – OOPS Banner Display Application
+ * OOPSBannerApp UC4 – OOPS Banner Display Application
  * @author Developer
- * @version 3.0
- * by using String.join() to align and format each row.
- * Instead of manually spacing everywhere.
+ * @version 4.0
+ * This version stores each row of the OOPS banner inside a String array and then prints it using a loop.
+ * The banner rows are created using String.join()
  **/
 class OOPSBannerApp{
 	public static void main(String[] args){
+
+		//rows are stored in the String array
+		String lines[]=new String[9];
+		lines[0]=String.join("   ","   ***   ","   ***   ","*******","   ***** ");
+		lines[1]=String.join("   "," **   ** "," **   ** ","**    *"," **      ");
+		lines[2]=String.join("   ","**     **","**     **","**    *","**       ");
+		lines[3]=String.join("   ","**     **","**     **","**    *"," **      ");
+		lines[4]=String.join("   ","**     **","**     **","*******","   ***   ");
+		lines[5]=String.join("   ","**     **","**     **","**     ","      ** ");
+		lines[6]=String.join("   ","**     **","**     **","**     ","       **");
+		lines[7]=String.join("   "," **   ** "," **   ** ","**     ","      ** ");
+		lines[8]=String.join("   ","   ***   ","   ***   ","**     "," *****   ");
 		
-		// Printing the banner row by row
-		System.out.println(String.join("   ","   ***   ","   ***   ","*******","   ***** "));
-		System.out.println(String.join("   "," **   ** "," **   ** ","**    *"," **      "));
-		System.out.println(String.join("   ","**     **","**     **","**    *","**       "));
-		System.out.println(String.join("   ","**     **","**     **","**    *"," **      "));
-		System.out.println(String.join("   ","**     **","**     **","*******","   ***   "));
-		System.out.println(String.join("   ","**     **","**     **","**     ","      ** "));
-		System.out.println(String.join("   ","**     **","**     **","**     ","       **"));
-		System.out.println(String.join("   "," **   ** "," **   ** ","**     ","      ** "));
-		System.out.println(String.join("   ","   ***   ","   ***   ","**     "," *****   "));
+		//printing row by row
+		for(String line:lines){
+			System.out.println(line);
+		}
 	}
 }
